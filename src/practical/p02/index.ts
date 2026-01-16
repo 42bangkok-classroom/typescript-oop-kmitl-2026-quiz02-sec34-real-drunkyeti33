ddeb390823326b6f64d7aa0e69a00be4c26d4b5d
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-01-16 19:56:22 +0700

    final

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 type newUser = {
   name: string;
   username?: string;
@@ -20,4 +21,74 @@ type newUser = {
     bs: string;
   };
 };
-export function addUser(newUser: newUser | null) {}
+
+interface Geo {
+  lat: string | null;
+  lng: string | null;
+}
+
+interface Address {
+  street: string | null;
+  suite: string | null;
+  city: string | null;
+  zipcode: string | null;
+  geo: Geo | null;
+}
+
+interface User {
+  id: number;
+  name: string | null;
+  phone: string | null;
+  address: Address | null;
+}
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+export async function addUser(
+  newUserData: newUser | null
+): Promise<User[]> {
+  try {
+    const response = await axios.get<User[]>(API_URL);
+    const users = response.data;
+
+    const result: User[] = users.map((ggez) => ({
+      id: ggez.id,
+      name: ggez.name ?? null,
+      phone: ggez.phone ?? null,
+      address: ggez.address ?? null,
+    }));
+
+    if (!newUserData) {
+      return result;
+    }
+
+    const lastId = result.length
+      ? result[result.length - 1].id
+      : 0;
+
+    const address: Address | null = newUserData.address
+      ? {
+          street: newUserData.address.street ?? null,
+          suite: newUserData.address.suite ?? null,
+          city: newUserData.address.city ?? null,
+          zipcode: newUserData.address.zipcode ?? null,
+          geo: newUserData.address.geo
+            ? {
+                lat: newUserData.address.geo.lat ?? null,
+                lng: newUserData.address.geo.lng ?? null,
+              }
+            : null,
+        }
+      : null;
+
+    const newUser: User = {
+      id: lastId + 1,
+      name: newUserData.name ?? null,
+      phone: newUserData.phone ?? null,
+      address,
+    };
+
+    return [...result, newUser];
+  } catch {
+    return [];
+  }
+}
